7f9d0faf134a47e30f654aabb7f100ddd8f38cee
Lukasz Sroczynski <lukasz.sroczynski@allegro.pl>
2018-03-22 21:44:07 +0100

    db and basic setup for web

--- src/main/kotlin/pl/braintelligence/examples/netflux/controllers/MovieController.kt
+++ src/main/kotlin/pl/braintelligence/examples/netflux/controllers/MovieController.kt
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pl.braintelligence.examples.netflux.domain.Movie
 import pl.braintelligence.examples.netflux.domain.MovieEvent
 import pl.braintelligence.examples.netflux.service.MovieService
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.lang.invoke.MethodHandles
 
 @RestController
@@ -17,14 +19,24 @@ class MovieController(
 ) {
 
     @GetMapping(value = ["/{id}/events"])
-    fun streamMovieEvents(
-            @PathVariable id: String
-    ): Flux<MovieEvent> {
+    fun streamMovieEvents(@PathVariable id: String): Flux<MovieEvent> {
 
-        logger.info("Movie with id: $id")
-
+        logger.info("Movies with: $id")
         return movieService.events(id)
     }
+
+    @GetMapping(value = ["/{id}"])
+    fun getMovieById(@PathVariable id: String): Mono<Movie> {
+
+        return movieService.getMovieById(id)
+    }
+
+    @GetMapping
+    fun getAllMovies(): Flux<Movie> {
+
+        return movieService.getAllMovies()
+    }
+
 
     companion object {
         private val logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
